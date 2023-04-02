f124d5f4b9d3380afedf99ba1772759d97589fab
furry33 <60417722+jokeryy5@users.noreply.github.com>
2023-04-02 11:49:52 +0630

    Add files via upload

---
+++ bank.c
@@ -0,0 +1,26 @@
+//
+// Created by ADMIN on 12-Feb-23.
+//
+#include "stdio.h"
+#include "online_bank1.h"
+
+int main(){
+
+     space_counter();
+    loadingAllDataFromFile();
+//    printingAllData();
+//    welcome();
+
+
+//     get_time();
+//    printf("Current edited time: %s", Ctime[0].c_Time);
+//    integer_to_char(1000);
+
+    current_data_toTransfer(3000);
+     calculate_amounts_same_days(0);
+//    get_record_AandT(0);
+
+//    get_record_AandT(0);
+    return 0;
+
+}
